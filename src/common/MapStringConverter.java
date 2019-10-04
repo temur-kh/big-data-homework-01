56c2c40f8c36139b0cc04a3072572ff135c6852b
Temur <tim_1998@bk.ru>
2019-10-04 23:22:32 +0300

    Relevance analizator (#3)
    * Add two more lambda declarations for parsing to IntWritable and DoubleWritable
    
    * Refactor code of MapStringConverter class
    
    * Write a initial implementation of the Relevance Analizator module of the Ranger Engine

--- src/common/MapStringConverter.java
+++ src/common/MapStringConverter.java
@@ -1,16 +1,21 @@
 package common;
 
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 public class MapStringConverter {
-    public static final String KVSeparator = "=";
-    public static final String PairSeparator = ";";
+    private static final String KVSeparator = "=";
+    private static final String PairSeparator = ";";
 
     public static final FromString<Double> parseDouble = Double::parseDouble;
     public static final FromString<Integer> parseInt = Integer::parseInt;
     public static final FromString<String> parseString = string -> string;
+    public static final FromString<IntWritable> parseIntWritable = string -> new IntWritable(Integer.parseInt(string));
+    public static final FromString<DoubleWritable> parseDoubleWritable = string -> new DoubleWritable(Integer.parseInt(string));
 
     private MapStringConverter() {
     }
@@ -19,7 +24,7 @@ public class MapStringConverter {
         return key.toString() + KVSeparator + value.toString();
     }
 
-    public static String mergeStringPairs(List<String> pairs) {
+    private static String mergeStringPairs(List<String> pairs) {
         if (pairs.isEmpty()) {
             return "";
         }
@@ -27,11 +32,11 @@ public class MapStringConverter {
         if (size == 1) {
             return pairs.get(0);
         }
-        String out = pairs.get(0);
+        StringBuilder out = new StringBuilder(pairs.get(0));
         for (int i = 1; i < size; ++i) {
-            out += PairSeparator + pairs.get(i);
+            out.append(PairSeparator).append(pairs.get(i));
         }
-        return out;
+        return out.toString();
     }
 
     public static <K, V> String map2String(HashMap<K, V> map) {
@@ -43,10 +48,10 @@ public class MapStringConverter {
     }
 
     public static class Pair<K, V> {
-        public K key;
-        public V value;
+        K key;
+        V value;
 
-        public Pair(K key, V value) {
+        Pair(K key, V value) {
             this.key = key;
             this.value = value;
         }
@@ -60,11 +65,11 @@ public class MapStringConverter {
         T convert(String string);
     }
 
-    public static <K, V> Pair<K, V> string2Pair(String string, FromString<K> k2str, FromString<V> v2str) {
+    private static <K, V> Pair<K, V> string2Pair(String string, FromString<K> k2str, FromString<V> v2str) {
         String[] s = string.split(KVSeparator);
         K key = k2str.convert(s[0]);
         V value = v2str.convert(s[1]);
-        return new Pair<K, V>(key, value);
+        return new Pair<>(key, value);
     }
 
     public static <K, V> HashMap<K, V> string2Map(String string, FromString<K> k2str, FromString<V> v2str) {
