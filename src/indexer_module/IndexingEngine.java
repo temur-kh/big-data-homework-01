c49bc3d0a03d1280025b47b9f130872cbc402139
Temur <tim_1998@bk.ru>
2019-10-05 22:25:48 +0300

    Combine Indexing engine (#13)
    * Update DocumentCounter and WordEnumerator classes to have run methods
    
    * Add the initial implementation of Indexing Engine class that will be used to run the whole engine
    
    * Fix bugs and update the run method
    
    * Update the run method in CorpusParser and IndexingEngine
    
    * Refactor code, change String -> Path, fix bugs

---
+++ src/indexer_module/IndexingEngine.java
@@ -0,0 +1,20 @@
+package indexer_module;
+
+import common.CorpusParser;
+import org.apache.hadoop.fs.Path;
+
+public class IndexingEngine {
+    public static void main(String[] args) throws Exception {
+        // input corpus files directory
+        Path corpusPath = new Path(args[1]);
+        // directory for job outputs to save in
+        Path outputDir = new Path(args[2]);
+
+        Path parsedCorpusPath = CorpusParser.run(corpusPath, outputDir, CorpusParser.PARSE_TEXT);
+        Path parsedCorpusURLTitlePath = CorpusParser.run(corpusPath, outputDir, CorpusParser.PARSE_URL_TITLE);
+        Path docCountPath = DocumentCounter.run(parsedCorpusPath, outputDir);
+        Path wordEnumPath = WordEnumerator.run(parsedCorpusPath, outputDir);
+        Path docVectorsPath = Indexer.run(parsedCorpusPath, wordEnumPath, docCountPath, outputDir);
+
+    }
+}
