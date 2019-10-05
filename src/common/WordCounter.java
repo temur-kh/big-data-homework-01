93e59e9fd05efe8ff3483907487adb3bd5e4f17c
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2019-10-05 19:54:08 +0300

    Sv indexer (#11)
    * Implemented Indexer class and IndexerMapper. Working on IndexerReducer.
    Added Map2String convert class.
    
    * Reducer
    Little updates on MapStringConverter.
    Completed IndexerReducer.
    
    * Updated MapStringConverter.
    Added WordCounter.
    Updated Indexer and IndexerMapper.
    Fixed IndexerReducer.
    
    * Returned deleted files.

---
+++ src/common/WordCounter.java
@@ -0,0 +1,20 @@
+package common;
+
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class WordCounter {
+    public static HashMap<String, Integer> count(String string) {
+        StringTokenizer tokens = new StringTokenizer(string);
+        HashMap<String, Integer> map = new HashMap<>();
+        while (tokens.hasMoreTokens()) {
+            String word = tokens.nextToken();
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        return map;
+    }
+}
